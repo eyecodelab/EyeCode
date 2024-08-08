62250cf11bd171efdb9e02aff9d71e75aa479c81
eyecodelab <eyecodelab@gmail.com>
2024-08-08 21:36:06 +0800

    update

--- mailing/mailingvariables.php
+++ mailing/mailingvariables.php
@@ -2,6 +2,6 @@
     $mail_host = "smtp.gmail.com";
     $mail_port = "587";
     $mail_sender_email = "eyecodelab@gmail.com"; //sender
-    $mail_sender_password = "Dvm@98975989"; //sender
+    $mail_sender_password = "atbxucinerxqmlku"; //sender
     $mail_sender_name = "Website Form";
 ?>
